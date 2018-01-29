65012b5f64d4125ceb1c568d7889dd9ad432b930
Joseph Ogiolu <ogiolu@systemspecs.com.ng>
2018-01-29 20:42:52 +0100

    update

---
+++ src/main/java/com/joebas/customerservice/CustomerRelationShipServiceApplication.java
@@ -0,0 +1,12 @@
+package com.joebas.customerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerRelationShipServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerRelationShipServiceApplication.class, args);
+	}
+}
