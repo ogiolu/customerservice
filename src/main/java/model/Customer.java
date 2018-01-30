a9a45f003fc1365b3bece343f516e7bd67deab45
Joseph Ogiolu <ogiolu@systemspecs.com.ng>
2018-01-30 20:49:06 +0100

    #updates

--- src/main/java/model/Customer.java
+++ src/main/java/model/Customer.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 
@@ -17,6 +18,9 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class Customer {
     @Id
    private ObjectId _id_;
+    private PersonalInformation personalinfo;
+
+
 
 
 }
--- src/main/java/model/PersonalInformation.java
+++ src/main/java/model/PersonalInformation.java
@@ -3,17 +3,20 @@ package model;
 import lombok.*;
 
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
 public class PersonalInformation {
-    private String id_;
     private String firstName;
     private String lastName;
     private String middleName;
     private Date dateOfBirth;
-    private String phoneNumber;
-    private String email;
+    private String gender;
+    private List<String> phoneNumber= new ArrayList<String>();
+    private List<String>  email =new ArrayList<String>();
 
 }
